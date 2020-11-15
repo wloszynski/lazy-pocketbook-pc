43756e8c00300b048be1e1749ae5666cd777c0e7
wloszynski <adrian@wloszynski.pl>
2020-11-15 20:53:41 +0100

    forward, backward, home, options added

--- lazy_pocketbook.py
+++ lazy_pocketbook.py
@@ -1,25 +1,50 @@
 import paramiko
+import pyxhook
 
-host = '169.254.0.1'
-port = 22
-username = 'root'
-password = '1257'
+try:
+    host = 'ip'
+    port = 22
+    username = 'root'
+    password = 'password'
 
-forward = 'cat f.txt > /dev/input/event0'
-backward = 'cat f.txt > /dev/input/event0'
-home = 'cat h.txt > /dev/input/event0'
-option = 'cat o.txt > /dev/input/event0'
+    forward = 'cat f.txt > /dev/input/event0'
+    backward = 'cat b.txt > /dev/input/event0'
+    home = 'cat h.txt > /dev/input/event0'
+    option = 'cat o.txt > /dev/input/event0'
 
-command = forward
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh = paramiko.SSHClient()
+    ssh.connect(host, port, username, password)
 
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print('Connected')
 
-ssh.connect(host, port, username, password)
+except Exception:
+    print('Lack of connection, session aborted')
+    exit()
 
-stdin, stdout, stderr = ssh.exec_command(command)
 
-line = stdout.readlines()
+def OnKeyPress(event):
+    print (event.Key)
 
-print(line)
+    # press 1 to forward
+    if event.Ascii == 49:
+        ssh.exec_command(forward)
+    # press 2 to backward
+    elif event.Ascii == 50:
+        ssh.exec_command(backward)
+    # press 3 to home
+    elif event.Ascii == 51:
+        ssh.exec_command(home)
+    # press 4 to option
+    elif event.Ascii == 52:
+        ssh.exec_command(option)
+    # press 5 to exit
+    elif event.Ascii == 53:
+        exit()
+
+
+hm = pyxhook.HookManager()
+hm.KeyDown = OnKeyPress
+hm.HookKeyboard()
+hm.start()
